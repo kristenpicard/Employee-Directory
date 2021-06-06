6f4b344a8f1558005d37e734a689db63cd4ebec5
Kristen Picard <kristenpicard93@gmail.com>
2021-06-05 20:12:54 -0400

    fixing some styling

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,9 +2,13 @@ import axios from "axios";
 
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
-export default {
+let test = {
   getRandomEmployees: function() {
-    return axios.get("https://randomuser.me/api/?results=5");
+    return axios.get("https://randomuser.me/api/?results=15&nat=us");
   },
 };
+
+export default test
+
+console.log(test);
 
