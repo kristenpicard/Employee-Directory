ad0627ed26ef7180ae14fbe99ab8d7dcc79a0f5a
Kristen Picard <kristenpicard93@gmail.com>
2021-06-06 16:36:14 -0400

    Got search by first or last name working

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -7,27 +7,12 @@ function SearchForm(props) {
       <div className="form-group">
         <label htmlFor="employee">Search by Employee Name: </label>
         <input
-          value={props.search}
-          onChange={props.handleInputChange}
+          onChange={(event) => props.startSort(event)}
           name="employee"
-          list="employees"
           type="text"
           className="form-control"
           placeholder="Search Employee"
-          id="employee"
         />
-        {/* <datalist id="employees">
-          {props.employees.map((employee) => (
-            <option value={employee} key={employee} />
-          ))}
-        </datalist> */}
-        <button
-          type="submit"
-          onClick={props.handleFormSubmit}
-          className="btn btn-success"
-        >
-          Search
-        </button>
       </div>
     </form>
   );
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,86 +1,84 @@
 import React, { Component } from "react";
+import API from "../../utils/API";
 import EmployeeCard from "../EmployeeCard";
 import SearchForm from "../SearchForm";
-import SearchResults from "../SearchResults";
-import API from "../../utils/API";
 
 class Worker extends Component {
   state = {
     search: "",
     employees: [],
     results: [],
-    error: ""
+    sorted: false,
   };
 
-  // When the component mounts, get a list of all available employees and update state
+  // When the component mounts, get a list of all available employees and set state
   componentDidMount() {
-    API.getRandomEmployees()
-    .then((res) => {
+    API.getRandomEmployees().then((res) => {
       this.setState({
-        employees: res.data.results.map((event) => ({
-          firstName: event.name.first,
-          lastName: event.name.last,
-          picture: event.picture.large,
-          email: event.email,
-          phone: event.phone,
-          city: event.location.city,
-          state:event.location.state
-        })),
+        employees: res.data.results,
       });
-    })
-    .catch((err) => console.log(err));}
+    });
+  }
 
-  // searchEmployee = (filter) => {
-  //   console.log("Search", filter);
-  //   const filteredList = this.state.employees.filter((employee) => {
-  //     // merge data together, then check to see if employee exists
-  //     let values = Object.values(employee).join("").toLowerCase();
-  //     return values.indexOf(filter.toLowerCase()) !== -1;
-  //   });
-  //   // Update the employee list with the filtered value
-  //   this.setState({ employees: filteredList });
-  // };
-
-  handleInputChange = (event) => {
-    this.setState({ search: event.target.value });
+  // Sorts through employees based on what is typed in search bar
+  sortEmployees = () => {
+    let { employees, search} = this.state;
+    let results = employees.filter((sorted) => {
+      return (
+        sorted.name.first.toLowerCase().includes(search.toLowerCase()) ||
+        sorted.name.last.toLowerCase().includes(search.toLowerCase())
+      );
+    });
+    this.setState({ results });
   };
 
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    API.getRandomEmployees(this.state.search)
-      .then((res) => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
-        this.setState({ results: res.data.message, error: "" });
-      })
-      .catch((err) => this.setState({ error: err.message }));
+  // Dependent on what is typed, sends to SortEmployees to be filtered
+  startSorting = (event) => {
+    this.setState({ search: event.target.value }, () => {
+      this.sortEmployees();
+      this.setState({ sorted: true });
+    });
   };
-  render() {
+
+  render = () => {
     return (
       <div>
-        <h1 className="text-center">Search By Name</h1>
-        <SearchForm
-          handleFormSubmit={this.handleFormSubmit}
-          handleInputChange={this.handleInputChange}
-          employees={this.state.employees}
-        />
-        <SearchResults results={this.state.results} />
-        {[...this.state.employees].map((item) => (
-          <EmployeeCard
-            picture={item.picture}
-            firstName={item.firstName}
-            lastName={item.lastName}
-            email={item.email}
-            phone={item.phone}
-            city={item.city}
-            state={item.state}
-            key={item.key}
-          />
-        ))}
+        <div>
+          <SearchForm startSort={this.startSorting} />
+        </div>
+
+        <div>
+          {/* If the state is not "sorted", then map and render random employees */}
+          {!this.state.sorted
+            ? this.state.employees.map((item) => (
+                <EmployeeCard
+                  picture={item.picture.large}
+                  firstName={item.name.first}
+                  lastName={item.name.last}
+                  email={item.email}
+                  phone={item.phone}
+                  city={item.location.city}
+                  state={item.location.state}
+                  key={item.id.value}
+                />
+              ))
+            // If the state is "sorted", then take the results of the sort and display the corresponding card
+            : this.state.results.map((item) => (
+                <EmployeeCard
+                picture={item.picture.large}
+                firstName={item.name.first}
+                lastName={item.name.last}
+                email={item.email}
+                phone={item.phone}
+                city={item.location.city}
+                state={item.location.state}
+                key={item.id.value}
+                />
+              ))}
+        </div>
       </div>
     );
-  }
+  };
 }
 
 export default Worker;
