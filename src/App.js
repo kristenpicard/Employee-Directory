5225632eb6fb05c1bd6ed0a22926fd866553b17d
Kristen Picard <kristenpicard93@gmail.com>
2021-06-07 18:30:07 -0400

    Changing App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import Title from "./components/Title";
 import Wrapper from "./components/Wrapper";
 import Worker from "./components/Work";
-import test from "./utils/API";
 
 function App() {
   return (
     <Wrapper>
       <Title>Employee List</Title>
-      <Worker data={test}/>
+      <Worker />
     </Wrapper>
   );
 }
